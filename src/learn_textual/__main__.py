4535c0a2b2e6444614138c6ca00ff488c376bd80
Boni Lindsley <boni@lindsley.xyz>
2022-06-02 18:07:39 +0100

    add: tree example

--- src/learn_textual/__main__.py
+++ src/learn_textual/__main__.py
@@ -11,6 +11,7 @@ import rich.text
 import textual.app
 import textual.events
 import textual.widget
+import textual.widgets
 
 
 async def shutdown_app(app: textual.app.App) -> None:
@@ -47,9 +48,31 @@ class MessageArea(textual.widget.Widget):
             await shutdown_app(self.app)
 
 
+class TreeControl(textual.widgets.TreeControl[str]):
+    def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
+        super().__init__(*args, **kwargs)
+        self.show_cursor = True
+
+    def watch_show_cursor(self, value: bool) -> None:
+        if value:
+            self.hover_node = self.cursor
+        super().watch_show_cursor(value)
+
+    def watch_cursor_line(self, value: int) -> None:
+        if self.show_cursor:
+            self.hover_node = self.cursor
+        super().watch_cursor_line(value)
+
+    async def handle_tree_click(self, message: textual.widgets.TreeClick[str]) -> None:
+        node = message.node
+        if node.children:
+            await node.toggle()
+
+
 class App(textual.app.App):
     def __init__(self, *args: typing.Any, **kwargs: typing.Any) -> None:
         super().__init__(*args, **kwargs)
+        self._file_tree = TreeControl(label="/", data="/")
         self._message_area = MessageArea()
 
     async def on_key(self, event: textual.events.Key) -> None:
@@ -62,7 +85,17 @@ class App(textual.app.App):
             event.stop()
 
     async def on_mount(self) -> None:
+        file_tree = self._file_tree
+        await self.view.dock(file_tree, edge="top")
         await self.view.dock(self._message_area, edge="bottom", size=1)
+        await file_tree.focus()
+
+        # Example data.
+        await file_tree.root.add(label="home", data="home")
+        await file_tree.add(node_id=file_tree.id, label="alice", data="alice")
+        await file_tree.root.add(label="tmp", data="tmp")
+        await file_tree.root.add(label="usr", data="usr")
+        await file_tree.add(node_id=file_tree.id, label="lib", data="lib")
 
 
 async def async_main() -> int:
